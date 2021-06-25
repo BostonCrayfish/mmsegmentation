a9ab3aa5153a7fdd32b16ddd2611eba8cbed6955
Wang.F <geniusw@qq.com>
2021-06-25 10:55:50 +0800

    test

--- tools/train.py
+++ tools/train.py
@@ -171,7 +171,8 @@ def main():
     ########################### it is my test
     rand_input = torch.randn((4, 3, 512, 512))
     encoder_output = model.backbone(rand_input)
-    print(type(encoder_output))
+    print(len(encoder_output))
+    print(type(encoder_output[0]))
     print('shape of encoder output: {}'.format(encoder_output.shape()))
     head_output = model.decode_head(encoder_output)
     print('shape of head output:{}'.format(head_output.shape()))
