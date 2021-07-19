bbe19f8db8c328b0ae09cf72dc4f72e545c16f10
Wang.F <geniusw@qq.com>
2021-07-19 16:01:12 +0800

    test

--- moco/main_moco.py
+++ moco/main_moco.py
@@ -199,7 +199,7 @@ def main_worker(gpu, ngpus_per_node, args):
     criterion = nn.CrossEntropyLoss().cuda(args.gpu)
 
     # fix parameters in backbone
-    optimizer = torch.optim.SGD(model.module.decode_head.parameters(), args.lr,
+    optimizer = torch.optim.SGD(model.module.encoder_q.decode_head.parameters(), args.lr,
                                 momentum=args.momentum,
                                 weight_decay=args.weight_decay)
 
