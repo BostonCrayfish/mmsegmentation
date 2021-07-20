6666e11ba6ce37b142497c78ad68030f8b624ddb
Wang.F <geniusw@qq.com>
2021-07-20 19:55:24 +0800

    to run on different devices

--- configs/_base_/models/deeplabv3_r50-d16.py
+++ configs/_base_/models/deeplabv3_r50-d16.py
@@ -1,10 +1,19 @@
 # model settings
 # norm_cfg = dict(type='SyncBN', requires_grad=True)
 norm_cfg = dict(type='BN', requires_grad=True)
+
+# decide data directory by home name
+# please remove these lines and directly set data_root for your training
+import os
+if '/home/feng' in os.getcwd():
+    pretrain_path = '/home/feng/work_mmseg/checkpoints/moco_r50_200ep_trans.pth'
+elif '/home/cwei' in os.getcwd():
+    pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco_r50_200ep_trans.pth'
+else:
+    raise ValueError('unknown data directory')
 model = dict(
     type='EncoderDecoder',
-    # pretrained='/home/feng/work_mmseg/checkpoints/seg_moco_r50_d16_20ep.pth',
-    pretrained='/home/feng/work_mmseg/checkpoints/moco_r50_200ep_trans.pth',
+    pretrained=pretrain_path,
     backbone=dict(
         type='ResNet',
         depth=50,
