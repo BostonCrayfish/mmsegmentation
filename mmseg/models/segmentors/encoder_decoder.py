21d91d3057b733ee9bf86b9ce8b4144463172d97
Wang.F <geniusw@qq.com>
2021-08-18 16:04:42 +0800

    eval

--- mmseg/models/segmentors/encoder_decoder.py
+++ mmseg/models/segmentors/encoder_decoder.py
@@ -66,9 +66,9 @@ class EncoderDecoder(BaseSegmentor):
 
         super(EncoderDecoder, self).init_weights(pretrained)
         self.backbone.init_weights(pretrained=pretrained)
-        # self.decode_head.init_weights(
-        #     pretrained='/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/sss/sss_conv_0815.pth')
-        self.decode_head.init_weights()
+        self.decode_head.init_weights(
+            pretrained='/home/cwei/feng/work_mmseg/checkpoints/sss/sss_1bg_0818.pth')
+        # self.decode_head.init_weights()
         if self.with_auxiliary_head:
             if isinstance(self.auxiliary_head, nn.ModuleList):
                 for aux_head in self.auxiliary_head:
@@ -135,9 +135,9 @@ class EncoderDecoder(BaseSegmentor):
 
         return seg_logit
 
-    def forward(self, img):
-        x = self.extract_feat(img)
-        return self.decode_head.forward(x)
+    # def forward(self, img):
+    #     x = self.extract_feat(img)
+    #     return self.decode_head.forward(x)
 
     def forward_train(self, img, img_metas, gt_semantic_seg):
         """Forward function for training.
