19ef1d65c1bfaebde1decb0388da9628fc1e9102
Wang.F <geniusw@qq.com>
2021-10-09 17:37:27 +0800

    train on ccvl11

--- configs/my_config/entire_r50_aspp_d16_voc12.py
+++ configs/my_config/entire_r50_aspp_d16_voc12.py
@@ -9,8 +9,8 @@ if device_name == 'ccvl8':
     # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/sss/sss_1009.pth'
     data_root = '/home/cwei/feng/data/VOC2012'
 elif device_name == 'ccvl11':
-    # pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
-    pretrain_path = '/home/feng/work_mmseg/checkpoints/sss/sss_1009.pth'
+    pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
+    # pretrain_path = '/home/feng/work_mmseg/checkpoints/sss/sss_1009.pth'
     data_root = '/export/ccvl11b/cwei/data/VOC2012'
 elif device_name == 's2':
     pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/moco_r50_200ep_trans.pth'
