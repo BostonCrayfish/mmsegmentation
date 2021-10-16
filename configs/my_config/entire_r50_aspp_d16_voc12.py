9bb4162efe59ca17995cbecae10862796e1dbfc7
Wang.F <geniusw@qq.com>
2021-10-16 23:40:07 +0800

    eval on ccvl8

--- configs/my_config/entire_r50_aspp_d16_voc12.py
+++ configs/my_config/entire_r50_aspp_d16_voc12.py
@@ -3,18 +3,18 @@
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 # norm_cfg = dict(type='BN', requires_grad=True)
 
-device_name = 's6'
+device_name = 'ccvl8'
 if device_name == 'ccvl8':
-    pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
-    # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/sss/sss_1011.pth'
+    # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
+    pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/sss/sss_1016.pth'
     data_root = '/home/cwei/feng/data/VOC2012'
 elif device_name == 'ccvl11':
     pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
     # pretrain_path = '/home/feng/work_mmseg/checkpoints/sss/sss_1011.pth'
     data_root = '/export/ccvl11b/cwei/data/VOC2012'
 elif device_name == 's2':
-    pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/moco_r50_200ep_trans.pth'
-    # pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/sss/sss_1013.pth'
+    # pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/moco_r50_200ep_trans.pth'
+    pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/sss/sss_1016.pth'
     data_root = '/stor2/wangfeng/VOC2012'
 elif device_name == 's5':
     pretrain_path = '/home/user1/work_place/checkpoints/moco_r50_200ep_trans.pth'
@@ -28,7 +28,7 @@ else:
     raise ValueError('Unknown device')
 
 # pretrain_path = None
-channels = 256
+channels = 512
 
 model = dict(
     type='EncoderDecoder',
