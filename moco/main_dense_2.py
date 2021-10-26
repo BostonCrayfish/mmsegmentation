445dac491bdca61d06d4e58792c4893274abe8fa
Wang.F <geniusw@qq.com>
2021-10-26 11:28:30 +0800

    test dino 2.0

--- moco/main_dense_2.py
+++ moco/main_dense_2.py
@@ -374,7 +374,7 @@ def train(train_loader_list, model, criterion, optimizer, epoch, args):
         [batch_time, loss_m, loss_s, acc_moco, acc_seg],
         prefix="Epoch: [{}]".format(epoch))
 
-    for images, _ in train_loader:
+    for (images, _), (bg0, _), (bg1, _) in zip(train_loader, train_loader_bg0, train_loader_bg1):
         import matplotlib.pyplot as plt
         print(torch.where(images[0][0] == 0.)[0].shape)
         print(torch.where(images[1][0] == 0.)[0].shape)
@@ -382,6 +382,22 @@ def train(train_loader_list, model, criterion, optimizer, epoch, args):
         img1 = images[1][0].permute(1, 2, 0).numpy()
         plt.imsave('./img0.png', arr=img0, format='png')
         plt.imsave('./img1.png', arr=img1, format='png')
+
+        current_bs = images[0].size(0)
+        mask_idx_q = torch.where(images[0][:, 0, :, :] == 0.)
+        mask_idx_k = torch.where(images[0][:, 0, :, :] == 0.)
+        mask_q = torch.ones((current_bs, 224, 224))
+        mask_k = torch.ones((current_bs, 224, 224))
+        mask_q[mask_idx_q] = 0.
+        mask_k[mask_idx_k] = 0.
+        image_q = images[0] + torch.einsum('bcxy,bxy->bcxy', [bg0, 1 - mask_q])
+        image_k = images[1] + torch.einsum('bcxy,bxy->bcxy', [bg1, 1 - mask_k])
+
+        img_q = image_q[0].permute(1, 2, 0).numpy()
+        img_k = image_k[0].permute(1, 2, 0).numpy()
+        plt.imsave('./img_q.png', arr=img_q, format='png')
+        plt.imsave('./img_k.png', arr=img_k, format='png')
+
         raise
 
     cre_dense = nn.LogSoftmax(dim=1)
