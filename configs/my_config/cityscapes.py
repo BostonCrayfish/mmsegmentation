e933cbc630c4c0f6436d307e4bf691cd90a1ab9f
Wang.F <geniusw@qq.com>
2021-10-28 20:39:10 +0800

    cityscapes on ccvl11

--- configs/my_config/cityscapes.py
+++ configs/my_config/cityscapes.py
@@ -6,8 +6,8 @@ if device_name == 'ccvl8':
     pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/sss/sss_1023.pth'
     data_root = '/home/cwei/feng/data/cityscapes'
 elif device_name == 'ccvl11':
-    pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
-    # pretrain_path = '/home/feng/work_mmseg/checkpoints/sss/sss_1013.pth'
+    # pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
+    pretrain_path = '/home/feng/work_mmseg/checkpoints/sss/sss_1013.pth'
     data_root = '/home/feng/data/cityscapes'
 elif device_name == 's2':
     pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/moco_r50_200ep_trans.pth'
