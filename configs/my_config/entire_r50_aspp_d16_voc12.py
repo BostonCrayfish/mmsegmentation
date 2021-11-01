1d010b392090f5e8f090f624d14d822a1fe2f5e7
Wang.F <geniusw@qq.com>
2021-11-01 23:23:55 +0800

    train on s6

--- configs/my_config/entire_r50_aspp_d16_voc12.py
+++ configs/my_config/entire_r50_aspp_d16_voc12.py
@@ -1,9 +1,9 @@
 # model settings
 
-norm_cfg = dict(type='SyncBN', requires_grad=True)
-# norm_cfg = dict(type='BN', requires_grad=True)
+# norm_cfg = dict(type='SyncBN', requires_grad=True)
+norm_cfg = dict(type='BN', requires_grad=True)
 
-device_name = 'ccvl11'
+device_name = 's6'
 if device_name == 'ccvl8':
     # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
     pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_800ep_trans.pth'
