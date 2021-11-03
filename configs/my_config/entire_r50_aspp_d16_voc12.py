98b93db5b65c52ecac1b2fe92891a7dc30f0a3f8
Wang.F <geniusw@qq.com>
2021-11-03 13:30:46 +0800

    eval on ccvl11

--- configs/my_config/entire_r50_aspp_d16_voc12.py
+++ configs/my_config/entire_r50_aspp_d16_voc12.py
@@ -2,7 +2,8 @@
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 # norm_cfg = dict(type='BN', requires_grad=True)
 
-device_name = 's6'
+device_name = 'ccvl11'
+
 if device_name == 'ccvl8':
     pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
     # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_800ep_trans.pth'
@@ -10,7 +11,7 @@ if device_name == 'ccvl8':
     data_root = '/home/cwei/feng/data/VOC2012'
 elif device_name == 'ccvl11':
     # pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
-    pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_800ep_trans.pth'
+    pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/simclr_r50_200ep.pth'
     # pretrain_path = '/home/feng/work_mmseg/checkpoints/sss/sss_1027_ccvl11.pth'
     data_root = '/export/ccvl11b/cwei/data/VOC2012'
 elif device_name == 's2':
