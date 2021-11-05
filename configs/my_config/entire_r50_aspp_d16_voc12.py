4edd8fca28241566a9e6b69d6a0ef7ce5c30785c
Wang.F <geniusw@qq.com>
2021-11-05 21:55:36 +0800

    eval on s2

--- configs/my_config/entire_r50_aspp_d16_voc12.py
+++ configs/my_config/entire_r50_aspp_d16_voc12.py
@@ -2,7 +2,7 @@
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 # norm_cfg = dict(type='BN', requires_grad=True)
 
-device_name = 'ccvl11'
+device_name = 's2'
 
 if device_name == 'ccvl8':
     # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
@@ -16,8 +16,8 @@ elif device_name == 'ccvl11':
     pretrain_path = '/home/feng/work_mmseg/checkpoints/sss/byol_1ep_fine.pth'
     data_root = '/export/ccvl11b/cwei/data/VOC2012'
 elif device_name == 's2':
-    pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/moco_r50_200ep_trans.pth'
-    # pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/sss/sss_1031.pth'
+    # pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/moco_r50_200ep_trans.pth'
+    pretrain_path = '/home/qinghua-user3/deep-learning/work_mmseg/checkpoints/sss/sss_1105.pth'
     data_root = '/stor2/wangfeng/VOC2012'
 elif device_name == 's5':
     pretrain_path = '/home/user1/work_place/checkpoints/moco_r50_200ep_trans.pth'
