c69050053616def335d6f16704b5db96cc898caf
Wang.F <geniusw@qq.com>
2021-11-09 17:16:26 +0800

    eval on s6

--- configs/my_config/entire_r50_aspp_d16_voc12.py
+++ configs/my_config/entire_r50_aspp_d16_voc12.py
@@ -29,7 +29,7 @@ elif device_name == 's6':
     # pretrain_path = '/sdb1/fidtqh2/work_place/sss/moco_v1_r50_200ep_trans.pth'
     # pretrain_path = '/sdb1/fidtqh2/work_place/sss/insloc_c4_400ep_trans.pth'
     # pretrain_path = '/sdb1/fidtqh2/work_place/sss/sss_1013_ccvl11.pth'
-    pretrain_path = '/sdb1/fidtqh2/work_place/sss/sss_1028_s2.pth'
+    pretrain_path = '/sdb1/fidtqh2/work_place/sss/sss_scratch_no_dense.pth'
     data_root = '/sdb1/fidtqh2/data/VOC2012'
 else:
     raise ValueError('Unknown device')
