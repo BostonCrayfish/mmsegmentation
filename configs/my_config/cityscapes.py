50616b0ed50343a2e15b556158fecb0363e22748
Wang.F <geniusw@qq.com>
2021-11-10 17:43:39 +0800

    city of moco_800

--- configs/my_config/cityscapes.py
+++ configs/my_config/cityscapes.py
@@ -1,10 +1,10 @@
 # model settings
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 
-# pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_800ep_trans.pth'
+pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_800ep_trans.pth'
 # pretrain_path = '/home/feng/work_mmseg/checkpoints/sss/sss_1025.pth'
 # pretrain_path = 'open-mmlab://resnet50_v1c'
-pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/InfoMin_800ep_trans.pth'
+# pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/InfoMin_800ep_trans.pth'
 data_root = '/home/feng/data/cityscapes'
 
 channels = 512
@@ -109,7 +109,7 @@ workflow = [('train', 1)]
 cudnn_benchmark = True
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer = dict(type='SGD', lr=0.003, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
