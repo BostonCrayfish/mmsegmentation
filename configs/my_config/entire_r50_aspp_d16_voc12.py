2c8a49ceca498b842d314207e04bc969b8a4d4f3
Wang.F <geniusw@qq.com>
2021-11-11 15:02:47 +0800

    eval

--- configs/my_config/entire_r50_aspp_d16_voc12.py
+++ configs/my_config/entire_r50_aspp_d16_voc12.py
@@ -2,7 +2,7 @@
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 # norm_cfg = dict(type='BN', requires_grad=True)
 
-device_name = 's2'
+device_name = 's6'
 
 if device_name == 'ccvl8':
     pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
@@ -29,7 +29,7 @@ elif device_name == 's6':
     # pretrain_path = '/sdb1/fidtqh2/work_place/sss/moco_v1_r50_200ep_trans.pth'
     # pretrain_path = '/sdb1/fidtqh2/work_place/sss/insloc_c4_400ep_trans.pth'
     # pretrain_path = '/sdb1/fidtqh2/work_place/sss/sss_1013_ccvl11.pth'
-    pretrain_path = '/sdb1/fidtqh2/work_place/sss/sss_scratch_no_dense.pth'
+    pretrain_path = '/sdb1/fidtqh2/work_place/sss/r50_sup.pth'
     data_root = '/sdb1/fidtqh2/data/VOC2012'
 else:
     raise ValueError('Unknown device')
