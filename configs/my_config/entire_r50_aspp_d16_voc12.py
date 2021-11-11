8a6808dff72a91ce86ca06036d0a7928db37784b
Wang.F <geniusw@qq.com>
2021-11-11 15:20:29 +0800

    eval

--- configs/my_config/entire_r50_aspp_d16_voc12.py
+++ configs/my_config/entire_r50_aspp_d16_voc12.py
@@ -2,13 +2,13 @@
 norm_cfg = dict(type='SyncBN', requires_grad=True)
 # norm_cfg = dict(type='BN', requires_grad=True)
 
-device_name = 's6'
+device_name = 'ccvl8'
 
 if device_name == 'ccvl8':
-    pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
+    # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_200ep_trans.pth'
     # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/moco_r50_800ep_trans.pth'
     # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/moco/swav_r50_800ep_trans.pth'
-    # pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/sss/sss_1106.pth'
+    pretrain_path = '/home/cwei/feng/work_mmseg/checkpoints/sss/sss_1111.pth'
     data_root = '/home/cwei/feng/data/VOC2012'
 elif device_name == 'ccvl11':
     pretrain_path = '/home/feng/work_mmseg/checkpoints/moco/moco_r50_800ep_trans.pth'
@@ -155,7 +155,7 @@ workflow = [('train', 1)]
 cudnn_benchmark = True
 
 # optimizer
-optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+optimizer = dict(type='SGD', lr=0.003, momentum=0.9, weight_decay=0.0005)
 optimizer_config = dict()
 # learning policy
 lr_config = dict(policy='poly', power=0.9, min_lr=1e-4, by_epoch=False)
